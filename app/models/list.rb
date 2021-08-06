cfd3fc4e524522fd78fcca87ccf50606ef517265
Martin Masoni <marmasoni@gmail.com>
2021-08-06 15:12:46 +0200

    export

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,7 @@
 class List < ApplicationRecord
+  has_one_attached :photo
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, uniqueness: true, presence: true
+  validates :photo, presence: true
 end
